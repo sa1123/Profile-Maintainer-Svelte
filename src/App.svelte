7f8f6ec401efa314e6deab5ec49db775c5b15de9
sa1123 <sahil2398@yahoo.com>
2022-03-01 17:07:48 +0530

    1st commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	let people = [
+		{ name: 'Yoshi', beltColor: 'green', age: 25, id: 1},
+		{ name: 'Mario', beltColor: 'red', age: 30, id: 2}
+	];
+
+	const handleClick = (id) => {
+		people = people.filter((person) => person.id != id);
+	}
+
+</script>
+
+<main>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColor === 'black'}
+				<p><strong>Master Ninja</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColor} belt</p>
+			<button on:click={() => handleClick(person.id)}>Delete</button>
+		</div>
+	{:else}
+		<p>There are no people</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2 {
+		color: #123123;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 10000;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
