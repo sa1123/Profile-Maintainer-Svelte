1e4c5dfb7f606df76a72d7fd58a78c9db1b31b7b
sa1123 <sahil2398@yahoo.com>
2022-03-01 17:41:15 +0530

    CSS & Conditional Styles

--- src/App.svelte
+++ src/App.svelte
@@ -37,20 +37,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	h2 {
-		color: #123123;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 10000;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,9 +1,10 @@
 <script>
     let showModal = false;
+    let isPromo = true;
 </script>
 
 {#if showModal}
-<div class="backdrop">
+<div class="backdrop" class:promo={isPromo}>
     <div class="modal">
         <p>Sign up now!</p>
     </div>
@@ -11,5 +12,24 @@
 {/if}
 
 <style>
+    .backdrop{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.8);
+    }
 
+    .modal{
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: white;
+    }
+
+    .promo .modal{
+        background: crimson;
+        color: white;
+    }
 </style>
