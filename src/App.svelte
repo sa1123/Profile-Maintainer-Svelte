26a8485b9d3ea7ee18261eb931e49f9d2cf12b54
sa1123 <sahil2398@yahoo.com>
2022-03-02 11:45:38 +0530

    Forms 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Modal from './Modal.svelte';
+	import AddPersonForm from './AddPersonForm.svelte';
 
 	let showModal = false;
 
@@ -20,12 +21,7 @@
 </script>
 
 <Modal {showModal} on:click={toggleModal}>
-	<h3>Add a New Person</h3>
-	<form>
-		<input type="text" placeholder="name">
-		<input type="text" placeholder="belt color">
-		<button>Add Person</button>
-	</form>
+	<AddPersonForm/>
 </Modal>/>
 
 <main>
